67a58cef8a4c0d0734ae2d5d56f521ef4d0b694d
Ediz Züm <ediz.zum@hotmail.com>
2021-02-02 01:18:04 +0300

    Add project files.

---
+++ Control/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using System;
+
+namespace Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            int sayac = 1;
+            foreach (var xCar in carManager.GetAll())
+            {
+
+                System.Console.WriteLine(sayac + ". " + xCar.Brand + " -Günlük Fiyat: " + xCar.DailyPrice);
+                sayac++;
+            }
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,82 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _car;
+        public InMemoryCarDal()
+        {
+            _car = new List<Car>()
+            {
+                new Car()
+                {
+                    Id = 1, Brand = "Mercedes", Color = "Black", DailyPrice = 750, ModelYear = 2016,
+                    Description = "Like a brand new."
+                },
+                new Car()
+                {
+                    Id = 2, Brand = "BMW", Color = "Blue", DailyPrice = 650, ModelYear = 2015,
+                    Description = "Awesome for feeling excited."
+                },
+                new Car()
+                {
+                    Id = 3, Brand = "Opel", Color = "Red", DailyPrice = 400, ModelYear = 2016,
+                    Description = "Good for family."
+                },
+                new Car()
+                {
+                    Id = 4, Brand = "Fiat", Color = "Blue", DailyPrice = 300, ModelYear = 2017,
+                    Description = "Good for your budget."
+                },
+                new Car()
+                {
+                    Id = 5, Brand = "Peugeot", Color = "Red", DailyPrice = 350, ModelYear = 2014,
+                    Description = "For youth."
+                },
+                new Car()
+                {
+                    Id = 6, Brand = "Doblo", Color = "White", DailyPrice = 200, ModelYear = 2000,
+                    Description = "If you want to barbecue, just pick it."
+                }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _car.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _car.SingleOrDefault(c => c.Brand == car.Brand);
+            _car.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car;
+        }
+
+        public List<Car> GetById(int brandId, int colorId)
+        {
+            return _car.Where(c => c.Brand == c.Brand && c.Color == c.Color).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _car.SingleOrDefault(c => c.Brand == c.Brand);
+            carToUpdate.Id = car.Id;
+            carToUpdate.Brand = car.Brand;
+            carToUpdate.Color = car.Color;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
